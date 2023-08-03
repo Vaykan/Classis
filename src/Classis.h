36d64393e0e65f1590d883fc6e548e80d2eef49b
Vaykan <141342079+Vaykan@users.noreply.github.com>
2023-08-03 21:18:26 +0300

    Add files via upload

---
+++ src/Classis.h
@@ -0,0 +1,10 @@
+#ifndef CLASSIS_H_
+#define CLASSIS_H_
+int getNof();
+void createPeople(Person *people[], int nof);
+void getRandSeed();
+int getCorrectCinUnSignedInt();
+int getCorrectCinUnSignedInt2();
+
+
+#endif
---
+++ src/Fight.h
@@ -0,0 +1,8 @@
+#ifndef FIGHT_H_
+#define FIGHT_H_
+void startFight(int nof, Person *people[]);
+void fight(Person one, Person two);
+void endFight();
+
+
+#endif
